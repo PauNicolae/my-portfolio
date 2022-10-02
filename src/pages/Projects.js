a742ef6cfb4bb7d4e732779ac81ad162d95231ca
paunicolae <pa0pau14@yahoo.com>
2022-10-03 06:39:13 +0800

    added scrolls

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,61 +5,65 @@ import '../Project.css';
 
 function BasicExample() {
    return (
-      <div className="box">
-         <div className="row">
-          <div className="col-xs-12 col-sm col-md col-lg col-xl col-xxl  m-5">
-            <Card style={{ width: '18rem' }}>
-               <Card.Img variant="top" src="holder.js/100px180" />
-               <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                     Some quick example text to build on the card title and make
-                     up the bulk of the card's content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-               </Card.Body>
-            </Card>
+      <div>
+         <img className="scroll" src="../scroll.png" alt="" />
+
+         <div className="box">
+            <div className="row">
+               <div className="col-xs col-sm-6 col-md-6 col-lg-3 col-xl-3 col-xxl-3 p-3 ">
+                  <Card.Img variant="top" src="../project-scroll.png" />
+                  <Card className="card">
+                     <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                           Some quick example text to build on the card title
+                           and make up the bulk of the card's content.
+                        </Card.Text>
+                        <Button variant="success">Go somewhere</Button>
+                     </Card.Body>
+                  </Card>
+               </div>
+               <div className="col-xs col-sm-6 col-md-6 col-lg-3 col-xl-3 col-xxl-3 p-3">
+                  <Card.Img variant="top" src="../project-scroll.png" />
+                  <Card className="card">
+                     <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                           Some quick example text to build on the card title
+                           and make up the bulk of the card's content.
+                        </Card.Text>
+                        <Button variant="success">Go somewhere</Button>
+                     </Card.Body>
+                  </Card>
+               </div>
+               <div className="col-xs col-sm-6 col-md-6 col-lg-3 col-xl-3 col-xxl-3 p-3">
+                  <Card.Img variant="top" src="../project-scroll.png" />
+                  <Card className="card">
+                     <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                           Some quick example text to build on the card title
+                           and make up the bulk of the card's content.
+                        </Card.Text>
+                        <Button variant="success">Go somewhere</Button>
+                     </Card.Body>
+                  </Card>
+               </div>
+               <div className="col-xs col-sm-6 col-md-6 col-lg-3 col-xl-3 col-xxl-3 p-3">
+                  <Card.Img variant="top" src="../project-scroll.png" />
+                  <Card className="card">
+                     <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                           Some quick example text to build on the card title
+                           and make up the bulk of the card's content.
+                        </Card.Text>
+                        <Button variant="success">Go somewhere</Button>
+                     </Card.Body>
+                  </Card>
+               </div>
             </div>
-            <div className="col-xs col-sm col-md col-lg col-xl col-xxl  m-5">
-            <Card style={{ width: '18rem' }}>
-               <Card.Img variant="top" src="holder.js/100px180" />
-               <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                     Some quick example text to build on the card title and make
-                     up the bulk of the card's content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-               </Card.Body>
-            </Card>
-            </div>
-            <div className="col-xs col-sm col-md col-lg col-xl col-xxl  m-5">
-            <Card style={{ width: '18rem' }}>
-               <Card.Img variant="top" src="holder.js/100px180" />
-               <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                     Some quick example text to build on the card title and make
-                     up the bulk of the card's content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-               </Card.Body>
-            </Card>
-            </div>
-            <div className="col-xs col-sm col-md col-lg col-xl col-xxl  m-5">
-            <Card style={{ width: '18rem' }}>
-               <Card.Img variant="top" src="holder.js/100px180" />
-               <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                     Some quick example text to build on the card title and make
-                     up the bulk of the card's content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-               </Card.Body>
-            </Card>
          </div>
-      </div>
       </div>
    );
 }
