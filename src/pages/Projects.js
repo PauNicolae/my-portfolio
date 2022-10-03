b84a48abdf97c2b7aaa0f9b1225ec3b2ea75ca71
paunicolae <pa0pau14@yahoo.com>
2022-10-04 00:33:17 +0800

    fixed media query

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,60 +5,65 @@ import '../Project.css';
 
 function BasicExample() {
    return (
-      <div>
+      <div className="section">
          <img className="scroll" src="../scroll.png" alt="" />
 
          <div className="box">
             <div className="row">
-               <div className="col-xs col-sm-6 col-md-6 col-lg-3 col-xl-3 col-xxl-3 p-3 ">
-                  <Card.Img variant="top" src="../project-scroll.png" />
+               <div className="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3 p-3 ">
+                  <Card.Img variant="top" src="../kavogue-scroll.png" />
                   <Card className="card">
                      <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
                         <Card.Text>
-                           Some quick example text to build on the card title
-                           and make up the bulk of the card's content.
+                           Inspired Vogue Magazine website made of Bootstrap.
                         </Card.Text>
-                        <Button variant="success">Go somewhere</Button>
+                        <Card.Title>HTML, Bootstrap</Card.Title>
+                        <a className="site-button" href="https://paunicolae.github.io/kavogueOnlineMagazine/">Live Site</a>
                      </Card.Body>
                   </Card>
                </div>
-               <div className="col-xs col-sm-6 col-md-6 col-lg-3 col-xl-3 col-xxl-3 p-3">
-                  <Card.Img variant="top" src="../project-scroll.png" />
+               <div className="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3 p-3">
+                  <Card.Img variant="top" src="../cheater-scroll.png" />
                   <Card className="card">
                      <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
                         <Card.Text>
-                           Some quick example text to build on the card title
-                           and make up the bulk of the card's content.
+                           A Web Development Resource and Reference Website that
+                           features HTML tutorials, exercises, and tools such as
+                           a fully functional code editor.
                         </Card.Text>
-                        <Button variant="success">Go somewhere</Button>
+                        <Card.Title>HTML, CSS, Bootstrap</Card.Title>
+                        <a className="site-button" href="https://paunicolae.github.io/miniProject1/">Live Site</a>
                      </Card.Body>
                   </Card>
                </div>
-               <div className="col-xs col-sm-6 col-md-6 col-lg-3 col-xl-3 col-xxl-3 p-3">
-                  <Card.Img variant="top" src="../project-scroll.png" />
+               <div className="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3 p-3">
+                  <Card.Img variant="top" src="../nolimit-scroll.png" />
                   <Card className="card">
                      <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
                         <Card.Text>
-                           Some quick example text to build on the card title
-                           and make up the bulk of the card's content.
+                           A fully functional Fitness Website with Class
+                           bookings, Chat Bot, Exercises finder tutorial, Blogs,
+                           E-commerce and tools such as BMI and BMR Calculator.
                         </Card.Text>
-                        <Button variant="success">Go somewhere</Button>
+                        <Card.Title>
+                           HTML, CSS, Bootstrap, Javascript, API, jQuery
+                        </Card.Title>
+                        <a className="site-button" href="https://paunicolae.github.io/miniProject2/index.html">Live Site</a>
                      </Card.Body>
                   </Card>
                </div>
-               <div className="col-xs col-sm-6 col-md-6 col-lg-3 col-xl-3 col-xxl-3 p-3">
-                  <Card.Img variant="top" src="../project-scroll.png" />
+               <div className="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3 p-3">
+                  <Card.Img variant="top" src="../meralca-scroll.png" />
                   <Card className="card">
                      <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
                         <Card.Text>
-                           Some quick example text to build on the card title
-                           and make up the bulk of the card's content.
+                           A simple web application for easy viewing and payment
+                           of electric bills
                         </Card.Text>
-                        <Button variant="success">Go somewhere</Button>
+                        <Card.Title>
+                           HTML,CSS, Bootstrap, Javascript, API, jQuery, Laravel
+                        </Card.Title>
+                        <a className="site-button" href="#">Code</a>
                      </Card.Body>
                   </Card>
                </div>
